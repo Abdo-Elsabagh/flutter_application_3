edb3b50ee4ad45f1aa9ea2d9628ae322a8a6c530
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2023-11-22 23:41:08 +0200

    not comment

---
+++ lib/FirstScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstScrenn extends StatelessWidget {
+  const FirstScrenn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:const Column(
+        children: [],
+      ) ,
+    );
+  }
+}
