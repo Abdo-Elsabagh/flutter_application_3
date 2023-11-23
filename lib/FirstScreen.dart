759feee1f0c9cb6b13cd44cc71d0028a3d529117
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2023-11-23 03:25:32 +0200

    add 1

--- lib/FirstScreen.dart
+++ lib/FirstScreen.dart
@@ -7,9 +7,30 @@ class FirstScrenn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:const Column(
-        children: [],
-      ) ,
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(children: [
+          const Text('Best Caregorirs'),
+          SizedBox(
+            height: 100,
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const CircleAvatar(
+                    radius: 30,
+                    //backgroundColor: Colors.blue,
+                    backgroundImage: AssetImage('assets/d1.jpeg'),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    width: 15,
+                  );
+                },
+                itemCount: 15),
+          ),
+        ]),
+      ),
     );
   }
 }
